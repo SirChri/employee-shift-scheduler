6f5a17cb3b0af4a32c1fbadf672245256b69e52c
SirChri <christian.londero96@gmail.com>
2022-07-28 23:46:54 +0200

    code cleanup

--- server/app.js
+++ server/app.js
@@ -15,17 +15,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// We create a wrapper to workaround async errors not being transmitted correctly.
-function makeHandlerAwareOfAsyncErrors(handler) {
-    return async function(req, res, next) {
-        try {
-            await handler(req, res);
-        } catch (error) {
-            next(error);
-        }
-    };
-}
-
 // We provide a root route just as an example
 app.get('/', (req, res) => {
     res.send(`
